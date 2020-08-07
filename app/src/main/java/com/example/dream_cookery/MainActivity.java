55ee1550a0a0ad5aefa35758d422935c4f3fa4ff
Xiel7 <ericng402@gmail.com>
2020-08-07 17:07:42 +0800

    eric commit

--- app/src/main/java/com/example/dream_cookery/MainActivity.java
+++ app/src/main/java/com/example/dream_cookery/MainActivity.java
@@ -8,14 +8,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-//hjghghjgh
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//test
+    }
 
     public void helloWorld(View view) {
         Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
