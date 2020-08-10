fee0e4fad67a4fc9e7bab661cb2d17a78a0aa0e7
Xiel7 <ericng402@gmail.com>
2020-08-10 22:22:55 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/MainClassView.java
+++ app/src/main/java/com/example/dream_cookery/MainClassView.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainClassView extends AppCompatActivity {
 
@@ -11,7 +12,11 @@ public class MainClassView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_class_view);
-        
 
+    }
+
+    public void WesternClass(View view) {
+        Intent western = new Intent(this, WesternClass.class);
+        startActivity(western);
     }
 }
