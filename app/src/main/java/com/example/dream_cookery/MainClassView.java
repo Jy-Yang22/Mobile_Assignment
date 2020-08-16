adef5758d2c2aff6f6ed55ddfde0e911ce0eab28
Xiel7 <ericng402@gmail.com>
2020-08-16 23:44:28 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/MainClassView.java
+++ app/src/main/java/com/example/dream_cookery/MainClassView.java
@@ -1,22 +1,45 @@
 package com.example.dream_cookery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainClassView extends AppCompatActivity {
+
+    private ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_class_view);
 
+        menuIcon = findViewById(R.id.menuIcon);
+        menuIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                MenuFragment();
+            }
+        });
+
     }
 
     public void WesternClass(View view) {
         Intent western = new Intent(this, WesternClass.class);
         startActivity(western);
+    }
+
+    public void MenuFragment()
+    {
+        MenuFragment menuFragment = new MenuFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.flFragment, menuFragment).addToBackStack(null).commit();
     }
 }
