5bc36efb4fb322770795cd147acab081e7cf6177
HP <sswong1212@gmail.com>
2020-08-18 14:52:01 +0800

    commit by saiseng

---
+++ app/src/main/java/com/example/dream_cookery/aboutUsView.java
@@ -0,0 +1,14 @@
+package com.example.dream_cookery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutUsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
--- app/src/main/java/com/example/dream_cookery/profilePageView.java
+++ app/src/main/java/com/example/dream_cookery/profilePageView.java
@@ -3,6 +3,9 @@ package com.example.dream_cookery;
 import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class profilePageView extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class profilePageView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_items, menu);
+        return true;
+    }
+    public void onGroupItemClick(MenuItem item) {
+    }
+
 }
+
+
