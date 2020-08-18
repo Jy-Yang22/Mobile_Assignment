d195eb2bac4cd7819a3e0109c9fad99d1105b511
HP <sswong1212@gmail.com>
2020-08-18 15:44:31 +0800

    commit by saiseng

--- app/src/main/java/com/example/dream_cookery/aboutUsView.java
+++ app/src/main/java/com/example/dream_cookery/aboutUsView.java
@@ -2,7 +2,11 @@ package com.example.dream_cookery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class aboutUsView extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class aboutUsView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        ImageView img = (ImageView)findViewById(R.id.facebook);
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.facebook.com/"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView img2 = (ImageView)findViewById(R.id.email);
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://mail.google.com/"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView img3 = (ImageView)findViewById(R.id.phone);
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("tel:0123456789"));
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
