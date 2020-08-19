1c3b628d42bfc2da5604dc3ebfe6591df3c152b1
Xiel7 <ericng402@gmail.com>
2020-08-19 14:28:51 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/MenuFragment.java
+++ app/src/main/java/com/example/dream_cookery/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.example.dream_cookery;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MenuFragment extends Fragment {
+
+
 
 
     public MenuFragment() {
@@ -18,9 +22,21 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
+        TextView homeText = rootView.findViewById(R.id.home);
+        homeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToHome();
+            }
+        });
+        return rootView;
     }
 
+    public void goToHome()
+    {
+        Intent homeText = new Intent(getActivity(), MainActivity.class);
+        startActivity(homeText);
+    }
 
 }
