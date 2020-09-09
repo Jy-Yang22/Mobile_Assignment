54d4fe46ce7ef044b29afea6ec90104b32c8da1d
HP <sswong1212@gmail.com>
2020-09-09 21:21:19 +0800

    commit by saiseng

--- app/src/main/java/com/example/dream_cookery/editProfileView.java
+++ app/src/main/java/com/example/dream_cookery/editProfileView.java
@@ -1,18 +1,28 @@
 package com.example.dream_cookery;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class editProfileView extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    String[] gender = {"Male", "Female"};
 
-        String[] gender = {"Male", "Female"};
+    private static final String TAG = editProfileView.class.getSimpleName();
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
         @Override
         protected void onCreate (Bundle savedInstanceState)
@@ -27,8 +37,37 @@ public class editProfileView extends AppCompatActivity implements AdapterView.On
             aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
             spin.setAdapter(aa);
-        }//hello
 
+            mDisplayDate = (TextView)findViewById(R.id.profile_birth);
+            mDisplayDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar cal = Calendar.getInstance();
+                    int year = cal.get(Calendar.YEAR);
+                    int month = cal.get(Calendar.MONTH);
+                    int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                    DatePickerDialog dialog = new DatePickerDialog(
+                            editProfileView.this,
+                            android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                            mDateSetListener,
+                            year,month,day);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.show();
+                }
+            });
+
+            mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+                    Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                    String date = month + "/" + day + "/" + year;
+                    mDisplayDate.setText(date);
+                }
+            };
+        }
 
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id)
@@ -39,4 +78,7 @@ public class editProfileView extends AppCompatActivity implements AdapterView.On
     public void onNothingSelected(AdapterView<?> arg0)
     {
     }
+
+
+
 }
