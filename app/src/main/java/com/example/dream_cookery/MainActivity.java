d2a2cfdb5ed186bc1964c15354ca230789ad1be6
Xiel7 <ericng402@gmail.com>
2020-09-12 18:57:12 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/MainActivity.java
+++ app/src/main/java/com/example/dream_cookery/MainActivity.java
@@ -5,14 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen);
+
+        loginButton =  findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainClassView();
+            }
+        });
+
     }
 
     public void helloWorld(View view) {
