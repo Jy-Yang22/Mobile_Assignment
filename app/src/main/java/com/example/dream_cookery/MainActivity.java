0b59ad331a5f8521dda3cc873742949d1399b025
Xiel7 <ericng402@gmail.com>
2020-09-13 00:13:52 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/MainActivity.java
+++ app/src/main/java/com/example/dream_cookery/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
 
         loginButton =  findViewById(R.id.login_button);
+        registerLink = findViewById(R.id.register_link);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openMainClassView();
+            }
+        });
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterActivity();
             }
         });
 
@@ -35,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openMainClassView() {
         Intent intent = new Intent(this, editProfileView.class);
+        startActivity(intent);
+    }
+
+    public void openRegisterActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dream_cookery/RegisterActivity.java
+++ app/src/main/java/com/example/dream_cookery/RegisterActivity.java
@@ -1,14 +1,161 @@
 package com.example.dream_cookery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText inputEmail, inputUsername, inputPhoneNumber, inputPassword;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        createAccountButton = (Button) findViewById(R.id.register_button);
+        inputEmail = (EditText) findViewById(R.id.new_email_input_layout);
+        inputUsername = (EditText) findViewById(R.id.new_username_input_layout);
+        inputPhoneNumber = (EditText) findViewById(R.id.phone_number_input);
+        inputPassword = (EditText) findViewById(R.id.new_password_input_layout);
+        loadingBar = new ProgressDialog(this);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");*/
+                CreateAccount();
+            }
+        });
+
+    }
+
+    private void CreateAccount()
+    {
+        String email = inputEmail.getText().toString();
+        String username = inputUsername.getText().toString();
+        String phoneNum = inputPhoneNumber.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef;
+        myRef = database.getReference("users").child(username);
+        myRef.child("userID").setValue(username);
+        myRef.child("username").setValue(username);
+        myRef.child("email").setValue(email);
+        Toast.makeText(RegisterActivity.this, "yahoolooo", Toast.LENGTH_SHORT).show();*/
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please write your email...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(this, "Please write your username...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(phoneNum))
+        {
+            Toast.makeText(this, "Please write your phone number...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidateEmail(email, username, phoneNum, password);
+        }
+
+
+    }
+
+    private void openMainActivity()
+    {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void ValidateEmail(final String email, final String username, final String phoneNum, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (!(dataSnapshot.child("Users").child(email).exists()))
+                {
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("email", email);
+                    userdataMap.put("phone", phoneNum);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", username);
+
+                    RootRef.child("Users").child(email).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(RegisterActivity.this, "Congratulations, your account has been created.", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+
+                                        openMainActivity();
+                                    }
+                                    else
+                                    {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Network Error: Please try again after some time...", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "This " + email + " already exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please try again using another phone number.", Toast.LENGTH_SHORT).show();
+
+                    openMainActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loadingBar.dismiss();
+            }
+        });
     }
 }
