86769c55d099f7f7f895aab9816570b09dd683fc
Xiel7 <ericng402@gmail.com>
2020-09-15 15:42:31 +0800

    commit by Eric

--- app/src/main/java/com/example/dream_cookery/SuccessPayment.java
+++ app/src/main/java/com/example/dream_cookery/SuccessPayment.java
@@ -6,16 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.dream_cookery.Models.History;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SuccessPayment extends AppCompatActivity {
+
+    DatabaseReference HistoryRef;
+    FirebaseAuth firebaseAuth;
+    String currentID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_payment);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentID = firebaseAuth.getCurrentUser().getUid();
+
+        HistoryRef = FirebaseDatabase.getInstance().getReference("Users").child(currentID);
+        HistoryRef.child("cart").removeValue();
     }
 
     public void backhome(View view)
     {
+
         Intent home = new Intent(this, MainClassView.class);
         startActivity(home);
     }
